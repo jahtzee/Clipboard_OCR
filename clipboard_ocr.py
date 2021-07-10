026503f5fcdd44b84a7206d4f9829e0e85cc6318
jahtzee <60423957+jahtzee@users.noreply.github.com>
2021-07-11 00:02:33 +0200

    add various preprocessing functions

--- clipboard_ocr.py
+++ clipboard_ocr.py
@@ -14,6 +14,7 @@ from PIL import ImageGrab
 import tempfile
 import pyperclip
 from infi.systray import SysTrayIcon
+import numpy as np
 
 # Definitions
 custom_config = r'--oem 3 -l ger+rus+eng --psm 6'
@@ -31,6 +32,7 @@ def OCRImageFromTemp(path):
     if (path == 'Error'):
         return 'Error - Check clipboard contents!'
     img = cv2.imread(path)
+    img = preprocessImage(img)
     string = str(pytesseract.image_to_string(img))
     os.remove(temp+'\ocr_tmp.PNG')
     string = string.replace("\f","")
@@ -40,6 +42,59 @@ def OCRImageFromTemp(path):
 def systrayOption(systray):
     path = saveImageFromClipboard()
     pyperclip.copy(OCRImageFromTemp(path))  
+
+def preprocessImage(image):
+    image = getGrayscale(image)
+    return image
+
+# get grayscale image
+def getGrayscale(image):
+    return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# noise removal
+def removeNoise(image):
+    return cv2.medianBlur(image,5)
+ 
+#thresholding
+def thresholding(image):
+    return cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+
+#dilation
+def dilate(image):
+    kernel = np.ones((5,5),np.uint8)
+    return cv2.dilate(image, kernel, iterations = 1)
+    
+#erosion
+def erode(image):
+    kernel = np.ones((5,5),np.uint8)
+    return cv2.erode(image, kernel, iterations = 1)
+
+#opening - erosion followed by dilation
+def opening(image):
+    kernel = np.ones((5,5),np.uint8)
+    return cv2.morphologyEx(image, cv2.MORPH_OPEN, kernel)
+
+#canny edge detection
+def canny(image):
+    return cv2.Canny(image, 100, 200)
+
+#skew correction
+def deskew(image):
+    coords = np.column_stack(np.where(image > 0))
+    angle = cv2.minAreaRect(coords)[-1]
+    if angle < -45:
+        angle = -(90 + angle)
+    else:
+        angle = -angle
+    (h, w) = image.shape[:2]
+    center = (w // 2, h // 2)
+    M = cv2.getRotationMatrix2D(center, angle, 1.0)
+    rotated = cv2.warpAffine(image, M, (w, h), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
+    return rotated
+
+#template matching
+def match_template(image, template):
+    return cv2.matchTemplate(image, template, cv2.TM_CCOEFF_NORMED) 
 
 if __name__ == "__main__":
     menu_options = (("Copy", None, systrayOption),)
